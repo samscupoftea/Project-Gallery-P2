280a64a1221b5a1c48794ffdecd32654ed5042cc
sam hockaday <samuel.hockaday@gmail.com>
2023-03-13 15:10:39 +0000

    Adding JS click functionality.

---
+++ script.js
@@ -0,0 +1,16 @@
+// target all project cards in project gallery using DOM 
+const projectCards = document.querySelector('.projectcard');
+
+// Here are all of the URL's for each project. 
+
+
+
+// Add event listener upon click. 
+
+projectCards.forEach((card, index) => {
+    card.EventListener('click', () => {
+
+        window.location.href = projectUrls[https://samscupoftea.github.io/My-First-Repo/];
+
+    })
+})
